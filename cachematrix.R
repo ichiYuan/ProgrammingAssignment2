d06e1c7bea0bb590ef25948674ce1c372d43f849
Elf <elf@ElfYuan-no-MacBook-Air.local>
2015-01-24 22:51:56 -0800

    origin

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrixInv <- NULL
+        set <- function(y) {
+                x <<- y
+                matrixInv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(Inv) matrixInv <<- Inv
+        getInv <- function() matrixInv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matrixInv <- x$getInv()
+        if(!is.null(matrixInv)) {
+                message("getting cached data")
+                return(matrixInv)
+        }
+        data <- x$get()
+        matrixInv <- solve(data, ...)
+        x$setInv(matrixInv)
+        matrixInv
 }
